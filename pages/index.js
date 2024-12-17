f984438e9c12924a7673eb1c9fda785969db1bcb
hungran <26101787+hungran@users.noreply.github.com>
2024-12-17 18:56:09 +0700

    css
    Signed-off-by: hungran <26101787+hungran@users.noreply.github.com>

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
+import styles from '../styles/Home.module.css';
+
 export default function Home() {
     return (
-      <div>
+      <div className={styles.container}>
         <h1>Welcome to AWS Cloud Practitioner Essentials.</h1>
         <p>Learn how to build, deploy, and manage applications in the AWS Cloud.</p>
+        <button className={styles.button}>Get Started</button>
       </div>
     );
-  }
-  
+}
